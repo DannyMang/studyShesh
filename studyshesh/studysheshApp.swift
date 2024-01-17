2cd0541af7a4b6e2ec88d7280e90ab229e731409
daniel ung <daniel.ung@sjsu.edu>
2024-01-16 19:11:09 -0800

    Initial Commit

---
+++ studyshesh/studysheshApp.swift
@@ -0,0 +1,17 @@
+//
+//  studysheshApp.swift
+//  studyshesh
+//
+//  Created by Daniel Ung on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct studysheshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
